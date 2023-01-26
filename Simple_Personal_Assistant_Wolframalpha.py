46357a798724dee5c03e3ac45219c4bbdbfa92c2
Simar Dhiman <itssimardhiman@gmail.com>
2023-01-26 23:05:10 +0530

    Add files via upload

---
+++ Simple_Personal_Assistant_Wolframalpha.py
@@ -0,0 +1,29 @@
+import wolframalpha
+
+# App id obtained by the above steps
+app_id = 'YX8QE4-HRAHJQR4PK'
+
+while True:
+    # Taking input from user
+    question = input('Question: ')
+
+    if question == 'exit':
+        break
+
+    try:
+        # Instance of wolfram alpha client class
+        client = wolframalpha.Client(app_id)
+
+        # Stores the response from wolfram alpha
+        res = client.query(question)
+
+        # Includes only text from the response
+        answer = next(res.results).text
+
+        print(answer)
+
+    except StopIteration:
+        print("No result found for the question: ", question)
+    except Exception as e:
+        print("Error: ", e)
+
